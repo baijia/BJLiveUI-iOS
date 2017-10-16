9138961739e33bfcf222fdb20ce2471b1216cd9a
iwill <minglq.9@gmail.com>
2017-10-16 23:40:37 +0800

    clean

--- demo-swift/BJLiveUISwiftDemo/BJLiveUISwiftDemo-Bridging-Header.h
+++
@@ -1,7 +0,0 @@
-//
-//  Use this file to import your target's public headers that you would like to expose to Swift.
-//
-
-#import <BJLiveCore/BJLiveCore.h>
-#import <BJLiveUI/BJLiveUI.h>
-#import "BJRoomViewController.h"
