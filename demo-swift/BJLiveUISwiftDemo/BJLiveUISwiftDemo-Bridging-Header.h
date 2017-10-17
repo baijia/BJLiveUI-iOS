998bb8a737009f4ac6364958299494f7d24c4fde
iwill <minglq.9@gmail.com>
2017-10-17 12:08:34 +0800

    swift demo

---
+++ demo-swift/BJLiveUISwiftDemo/BJLiveUISwiftDemo-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#import <BJLiveCore/BJLiveCore.h>
+#import <BJLiveUI/BJLiveUI.h>
+#import <ReactiveObjC/ReactiveObjC.h>
+#import "NSObject+SwiftObserver.h"
